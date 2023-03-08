efc57a8dc771b96c7d95bafe58cb208a7b832d1c
Rohit <suriyarohit1995@gmail.com>
2023-03-08 12:39:59 +0530

    just updated dummy to load data

--- app/src/main/java/com/example/stackviewdemo/UI/MainActivity.kt
+++ app/src/main/java/com/example/stackviewdemo/UI/MainActivity.kt
@@ -28,8 +28,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        loadData()
-        runDemo()
+        loadData()
+//        runDemo()
 
     }
 
